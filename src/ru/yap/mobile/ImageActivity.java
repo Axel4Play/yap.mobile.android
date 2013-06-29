85e5e19c5348c3d707d4218395ea6c5ec486b8de
axel4play@gmail.com <axel4play@gmail.com>
2013-06-29 21:51:42 +0600

    ImageActivity icon color.

--- src/ru/yap/mobile/ImageActivity.java
+++ src/ru/yap/mobile/ImageActivity.java
@@ -73,8 +73,8 @@ public class ImageActivity extends FragmentActivity {
 			);			
 		}
 		DisplayImageOptions options = new DisplayImageOptions.Builder()
-			.showStubImage(R.drawable.navigation_refresh_light)
-			.showImageOnFail(R.drawable.alerts_and_states_warning_light)
+			.showStubImage(R.drawable.navigation_refresh_dark)
+			.showImageOnFail(R.drawable.alerts_and_states_warning_dark)
 			.resetViewBeforeLoading()
 			.cacheOnDisc()
 			.build();
